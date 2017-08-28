f2b97a8dd59b57c2a6473f57476485e2a5862e4d
supermy <springclick@gmail.com>
2017-08-28 15:44:04 +0800

    增加规则文件浏览页面 http://127.0.0.1:8080/file-browser.html

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 20170828
+* 增加页面访问资源 http://127.0.0.1:8080/  对应 src/main/resources/static/目录
+* 在线发布与更新 dmn table;
+* 支持 drd;
+
--- src/main/java/org/camunda/bpm/spring/boot/example/dmn/rest/DmnRestApplication.java
+++ src/main/java/org/camunda/bpm/spring/boot/example/dmn/rest/DmnRestApplication.java
@@ -9,8 +9,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+//@EnableWebMvc
+@ComponentScan({"logviewer"})
 public class DmnRestApplication {
 
   public static void main(final String... args) throws Exception {
